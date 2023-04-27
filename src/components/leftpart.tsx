fae4bb52d3c0dbf12503f4b564756bc3052fdf8a
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-04-27 21:50:05 +0700

    video 26 - left part

--- src/components/leftpart.tsx
+++ src/components/leftpart.tsx
@@ -1,19 +1,20 @@
+import desktopLogo from '@/assets/img/logo/react.png';
+
 const LeftPart = () => {
     return (
         <>
             <div className="arlo_tm_leftpart_wrap">
                 <div className="leftpart_inner">
                     <div className="logo_wrap">
-                        <a href="#"><img src="img/logo/desktop-logo.png" alt="desktop-logo" /></a>
+                        <a href="#"><img src={desktopLogo} alt="desktop-logo" /></a>
                     </div>
                     <div className="menu_list_wrap">
                         <ul className="anchor_nav">
                             <li><a href="#home">Home</a></li>
                             <li><a href="#about">About</a></li>
-                            <li><a href="#services">Services</a></li>
+                            <li><a href="#services">Projects</a></li>
                             <li><a href="#portfolio">Portfolio</a></li>
-                            <li><a href="#news">News</a></li>
-                            <li><a href="#contact">Contact</a></li>
+
                         </ul>
                     </div>
                     <div className="leftpart_bottom">
