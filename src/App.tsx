db59bf592711424ebd71c07c8e25bed8ea49ac1e
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-04-27 20:49:10 +0700

    video 25 - setup alias path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import Portfolio from './screens/portfolio';
-import './styles/plugins.scss';
-import './styles/style.scss';
-
+import Portfolio from '@/screens/portfolio';
+import '@/styles/plugins.scss';
+import '@/styles/style.scss';
 function App() {
 
   return (
