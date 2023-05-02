d6a7b3875384825a9955dfb1a31c8fda5287ee43
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-05-02 14:08:53 +0700

    video 33 - contact component

--- src/components/leftpart.tsx
+++ src/components/leftpart.tsx
@@ -1,4 +1,8 @@
 import desktopLogo from '@/assets/img/logo/react.png';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { GrYoutube } from 'react-icons/gr';
+import { BsGithub, BsTiktok } from 'react-icons/bs';
+import { SiUdemy } from 'react-icons/si';
 
 const LeftPart = () => {
     return (
@@ -14,18 +18,31 @@ const LeftPart = () => {
                             <li><a href="#about">About</a></li>
                             <li><a href="#skills">Skills</a></li>
                             <li><a href="#project">Projects</a></li>
-                            <li><a href="#portfolio">Portfolio</a></li>
+                            <li><a href="#contact">Contact</a></li>
 
                         </ul>
                     </div>
                     <div className="leftpart_bottom">
                         <div className="social_wrap">
                             <ul>
-                                <li><a href="#"><i className="xcon-facebook"></i></a></li>
-                                <li><a href="#"><i className="xcon-twitter"></i></a></li>
-                                <li><a href="#"><i className="xcon-linkedin"></i></a></li>
-                                <li><a href="#"><i className="xcon-instagram"></i></a></li>
-                                <li><a href="#"><i className="xcon-behance"></i></a></li>
+                                <li>
+                                    <a href="https://www.udemy.com/user/eric-7039/" target='_blank' >
+                                        <SiUdemy />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/askITwithERIC" target='_blank'>
+                                        <FaFacebookSquare />
+                                    </a>
+                                </li>
+                                <li><a href="https://www.youtube.com/@hoidanit" target='_blank'><GrYoutube /></a></li>
+
+                                <li>
+                                    <a href="https://www.tiktok.com/@hoidanit" target='_blank'><BsTiktok /></a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/haryphamdev" target='_blank'><BsGithub /></a>
+                                </li>
                             </ul>
                         </div>
                     </div>
