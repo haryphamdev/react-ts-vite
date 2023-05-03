d81b80257bf51364a9427360d5755a4f45c1aa62
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-05-03 15:26:40 +0700

    video 42 - build with github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cv/',
   plugins: [react(), visualizer() as PluginOption],
   server: {
     port: 3000
